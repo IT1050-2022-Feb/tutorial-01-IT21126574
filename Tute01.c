b9d6f3f586ecd5b207c8ba0db73a33c2b6206e2e
IT21126574 <99212106+IT21126574@users.noreply.github.com>
2022-02-14 08:32:50 +0000

    Tutorial-1

--- Tute01.c
+++ Tute01.c
@@ -2,10 +2,30 @@
 
    Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
 
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-  
-  return 0;
+int main(void)//Have to calculate the sum and average
+{
+	int mark1,mark2,total_marks;
+	float avg;
+	
+	
+	printf("Enter mark one:");//input marks one
+	scanf("%d",&mark1);
+	
+	printf("Enter mark Two:");//input mmarks two
+	scanf("%d",&mark2);
+	
+	total_marks=mark1 + mark2;//Calculate total
+	
+	printf("Your Total marks is %d\n",total_marks);//display the total marks
+	
+	avg=(float)total_marks / 2;
+	
+	printf("Average of the above marks is : %.2f\n",avg);//print average marks
+	
+	return 0;
+	
 }
+
 
--- Tute04.c
+++ Tute04.c
@@ -4,7 +4,7 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 
 Do not change the code given in the main() function when you are implementing your solution.*/
 
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main() {
    int no1, no2;
@@ -16,4 +16,57 @@ int main() {
    printf("%d ", maximum(no1, no2));
    printf("%d ", multiply(no1, no2));
    return 0;
+}*/
+#include<stdio.h>
+
+int  minimum(int number1,int number2);
+int  maximum(int number1,int number2);
+int  multiply(int number1,int number2); 
+
+int main(void)
+
+{
+	int no1, no2;
+   
+    printf("Enter a value for no 1 : ");
+    scanf("%d", &no1);
+   
+    printf("Enter a value for no 2 : ");
+    scanf("%d", &no2);
+   
+    printf("%d ", minimum(no1, no2));
+    printf("%d ", maximum(no1, no2));
+    printf("%d ", multiply(no1, no2));
+   
+   return 0;
+}
+int  minimum(int number1,int number2)
+{
+	if(number1 >= number2)
+	{
+		return number1;
+	}
+	else
+	{
+		return number2;
+	}
+}
+int  maximum(int number1,int number2)
+{
+	if(number1 <= number2)
+	{
+		return number1;
+	}
+	else
+	{
+		return number2;
+	}
+}
+int  multiply(int number1,int number2)
+{
+	int answer;
+	
+	answer = number1 * number2;
+	
+	return answer;
 }
